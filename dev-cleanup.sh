fc20beca148e66947f8e4eb7b1e53dc03f77065e
rwbell900 <102238030+rwbell900@users.noreply.github.com>
2022-05-22 21:00:11 -0500

    updated readme file

--- dev-cleanup.sh
+++ dev-cleanup.sh
@@ -2,7 +2,14 @@
 
 [ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable"
 [ -z "$OWNER" ] && echo "Missing \$OWNER environment variable"
+echo --------------------------------------------
+echo
+echo "cleaning up the /neardev folder"
+echo
+rm -rf ./neardev
 
+# exit on first error after this point to avoid redeploying with successful build
+set -e
 echo "deleting $CONTRACT and setting $OWNER as beneficiary"
 echo
 near delete $CONTRACT $OWNER
